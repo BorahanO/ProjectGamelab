f7062e5a7ee296ecf709f1ca47f983d428f1e1c4
Bora Oksal <boraoxal@gmail.com>
2018-05-31 12:01:36 +0200

    Latest demo build

---
+++ Project/Ma100Ar/Assets/_Scripts/PortalCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+	public Transform playerCamera;
+	public Transform portal;
+	public Transform otherPortal;
+
+	void Update () {
+		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+		transform.position = portal.position + playerOffsetFromPortal;
+
+		float angleDifBetweenPortals = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+		Quaternion portalRotationDif = Quaternion.AngleAxis(angleDifBetweenPortals, Vector3.up);
+		Vector3 newCameraDirection = portalRotationDif * playerCamera.forward;
+		transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+	}
+}
