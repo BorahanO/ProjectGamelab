9ce86dee34930eab32fd2c4c6162ce783a6eee06
samjerry <jerry.sam@ziggo.nl>
2018-05-31 08:45:30 +0200

    Portal Added dto the game

---
+++ Project/Ma100Ar/Assets/PortalTextureSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+	public Camera camOtherWorld;
+
+	public Material camMatOtherWorld;
+
+
+	// Use this for initialization
+	void Start () {
+		if (camOtherWorld.targetTexture != null){
+			camOtherWorld.targetTexture.Release();
+		}
+		camOtherWorld.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		camMatOtherWorld.mainTexture = camOtherWorld.targetTexture;
+	}
+}
